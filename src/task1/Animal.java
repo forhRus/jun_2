b9ad92983ad6bbcff2a79a8fef7515b9d68c9e49
Sergey Shkaev <shkaev.ss@mail.ru>
2023-12-01 21:35:08 +0300

    переопределён toString()

--- src/task1/Animal.java
+++ src/task1/Animal.java
@@ -4,6 +4,14 @@ public abstract class Animal {
     private String name;
     private int age;
 
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
     public Animal(String name, int age) {
         this.name = name;
         this.age = age;
