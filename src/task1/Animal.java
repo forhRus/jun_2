3601917f5992cb7491394e0f10ed288cc362e8e4
Sergey Shkaev <shkaev.ss@mail.ru>
2023-12-01 21:16:17 +0300

    Добавленик конструктор

--- src/task1/Animal.java
+++ src/task1/Animal.java
@@ -3,4 +3,9 @@ package task1;
 public abstract class Animal {
     private String name;
     private int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
